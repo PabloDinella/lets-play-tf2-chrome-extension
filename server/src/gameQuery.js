2eacfd6627fe398c4fbba76318ea17a5a3a83df4
Pablo R. Dinella <pablo.dinella@gmail.com>
2020-03-13 13:57:36 -0300

    remove hardcoded condition, commentaries, and reduced payload size!

--- server/src/gameQuery.js
+++ server/src/gameQuery.js
@@ -3,17 +3,17 @@ const Sentry = require("@sentry/node");
 
 const SERVERS = [
   {
-    ip: "principal.awyeagames.website", // principal
+    ip: "principal.awyeagames.website",
     port: 27015,
     color: "red"
   },
   {
-    ip: "fast.awyeagames.website", // mvm
+    ip: "fast.awyeagames.website",
     port: 27016,
     color: "yellow"
   },
   {
-    ip: "mvm.awyeagames.website", // evento
+    ip: "mvm.awyeagames.website",
     port: 27015,
     color: "green"
   },
@@ -40,12 +40,11 @@ module.exports = {
       const state = await Promise.all(
         SERVERS.map(async ({ ip, port, ...rest }, index) => {
           try {
-            const serverInfo = await Gamedig.query({
+            const { raw, ...serverInfo } = await Gamedig.query({
               type: "tf2",
               host: ip,
               port
             });
-            if (index === 1) throw "Oops";
             return {
               ...serverInfo,
               ...rest
