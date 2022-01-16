fd99f2542a3e80d256606fc2e53ea284503d7f5e
PabloDinella <pablo.dinella@gmail.com>
2022-01-16 20:18:50 -0300

    Add v2 route and update servers

--- server/src/gameQuery.js
+++ server/src/gameQuery.js
@@ -5,49 +5,108 @@ const SERVERS = [
   {
     ip: "principal.awyeagames.website",
     port: 27015,
-    color: "red"
+    color: "red",
   },
   {
     ip: "principal2.awyeagames.website",
     port: 27015,
-    color: "orange"
+    color: "orange",
   },
   {
     ip: "euro.awyeagames.website",
     port: 27015,
-    color: "#B95F89"
+    color: "#B95F89",
   },
   {
     ip: "balance.awyeagames.website",
     port: 27016,
-    color: "#D5E2BC"
+    color: "#D5E2BC",
   },
   {
     ip: "fast.awyeagames.website",
     port: 27016,
-    color: "yellow"
+    color: "yellow",
   },
   {
     ip: "mvm.awyeagames.website",
     port: 27015,
-    color: "green"
+    color: "green",
   },
   {
     ip: "mvm.awyeagames.website",
     port: 27016,
-    color: "limegreen"
+    color: "limegreen",
   },
   {
     ip: "arena.awyeagames.website",
     port: 27015,
-    color: "indigo"
+    color: "indigo",
   },
   {
     ip: "arena.awyeagames.website",
     port: 27016,
-    color: "purple"
-  }
+    color: "purple",
+  },
 ];
+
+const SERVERS_V2 = {
+  normal: [
+    {
+      ip: "principal.awyeagames.website",
+      port: 27015,
+      color: "red",
+    },
+    {
+      ip: "fast.awyeagames.website",
+      port: 27016,
+      color: "yellow",
+    },
+    {
+      ip: "london.awyeagames.website",
+      port: 27015,
+      color: "#1F0F28",
+    },
+    {
+      ip: "miami.awyeagames.website",
+      port: 27015,
+      color: "#ef62b2",
+    },
+    {
+      ip: "sydney.awyeagames.website",
+      port: 27015,
+      color: "#f77247",
+    },
+    {
+      ip: "singapore.awyeagames.website",
+      port: 27015,
+      color: "#e62937",
+    },
+  ],
+  mvm: [
+    {
+      ip: "mvm.awyeagames.website",
+      port: 27015,
+      color: "green",
+    },
+    {
+      ip: "mvm.awyeagames.website",
+      port: 27016,
+      color: "limegreen",
+    },
+  ],
+  arena: [
+    {
+      ip: "arena.awyeagames.website",
+      port: 27015,
+      color: "indigo",
+    },
+    {
+      ip: "arena.awyeagames.website",
+      port: 27016,
+      color: "purple",
+    },
+  ],
+};
 
 module.exports = {
   async query() {
@@ -58,17 +117,17 @@ module.exports = {
             const { raw, ...serverInfo } = await Gamedig.query({
               type: "tf2",
               host: ip,
-              port
+              port,
             });
             return {
               ...serverInfo,
-              ...rest
+              ...rest,
             };
           } catch (error) {
             Sentry.captureException(error);
             return {
               error: `Não pôde obter informações de ${ip}:${port}`,
-              ...rest
+              ...rest,
             };
           }
         })
@@ -79,8 +138,48 @@ module.exports = {
     } catch (error) {
       Sentry.captureException(error);
       return {
-        error: "Não pôde obter informações dos servidores"
+        error: "Não pôde obter informações dos servidores",
       };
     }
-  }
+  },
+
+  async queryV2() {
+    try {
+      const state = await Promise.all(
+        Object.keys(SERVERS_V2).map(async (group) => {
+          return {
+            [group]: await Promise.all(
+              SERVERS_V2[group].map(async ({ ip, port, ...rest }, index) => {
+                try {
+                  const { raw, ...serverInfo } = await Gamedig.query({
+                    type: "tf2",
+                    host: ip,
+                    port,
+                  });
+                  return { ...serverInfo, ...rest };
+                } catch (error) {
+                  Sentry.captureException(error);
+                  return {
+                    error: `Não pôde obter informações de ${ip}:${port}`,
+                    ...rest,
+                  };
+                }
+              })
+            ),
+          };
+        })
+      );
+
+      Sentry.captureMessage("Requested succesfully");
+
+      return state.reduce((final, group) => {
+        return { ...final, ...group };
+      }, {});
+    } catch (error) {
+      Sentry.captureException(error);
+      return {
+        error: "Não pôde obter informações dos servidores",
+      };
+    }
+  },
 };
--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const Sentry = require('@sentry/node');
-Sentry.init({ dsn: `https://${process.env.sentryKey}@sentry.io/${process.env.sentryProject}` });
+const Sentry = require("@sentry/node");
+Sentry.init({
+  dsn: `https://${process.env.sentryKey}@sentry.io/${process.env.sentryProject}`,
+});
 
 app.use(
   cors({
-    origin: "*"
+    origin: "*",
   })
 );
 
@@ -16,5 +18,6 @@ const port = process.env.PORT || 3001;
 const gameQuery = require("./gameQuery");
 
 app.get("/", async (req, res) => res.json(await gameQuery.query()));
+app.get("/v2", async (req, res) => res.json(await gameQuery.queryV2()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
