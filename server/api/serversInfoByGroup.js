a9a77a1b5a474c04646b5c02444ee6909f4ae385
Pablo R. Dinella <pablo.dinella@gmail.com>
2023-02-06 10:01:43 -0300

    add log and update server list

--- server/api/serversInfoByGroup.js
+++ server/api/serversInfoByGroup.js
@@ -3,5 +3,7 @@ import gameQuery from "../src/gameQuery";
 export default async function handler(request, response) {
   const serversInfo = await gameQuery.queryV2();
 
+  console.log("loaded");
+
   response.status(200).json(serversInfo);
 }
--- server/src/gameQuery.js
+++ server/src/gameQuery.js
@@ -57,11 +57,6 @@ const SERVERS_V2 = {
       color: "red",
     },
     {
-      ip: "fast.awyeagames.website",
-      port: 27016,
-      color: "yellow",
-    },
-    {
       ip: "london.awyeagames.website",
       port: 27015,
       color: "#1F0F28",
@@ -71,16 +66,6 @@ const SERVERS_V2 = {
       port: 27015,
       color: "#ef62b2",
     },
-    {
-      ip: "sydney.awyeagames.website",
-      port: 27015,
-      color: "#f77247",
-    },
-    {
-      ip: "singapore.awyeagames.website",
-      port: 27015,
-      color: "#e62937",
-    },
   ],
   mvm: [
     {
@@ -88,22 +73,12 @@ const SERVERS_V2 = {
       port: 27015,
       color: "green",
     },
-    {
-      ip: "mvm.awyeagames.website",
-      port: 27016,
-      color: "limegreen",
-    },
   ],
   arena: [
     {
       ip: "arena.awyeagames.website",
       port: 27015,
       color: "indigo",
-    },
-    {
-      ip: "arena.awyeagames.website",
-      port: 27016,
-      color: "purple",
     },
   ],
 };
