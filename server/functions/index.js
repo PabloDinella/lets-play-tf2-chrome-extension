fb54e2d38e7438c643332076a887990cbeae3792
Pablo R. Dinella <pablo.dinella@gmail.com>
2023-02-06 11:51:29 -0300

    firebase setup

---
+++ server/functions/index.js
@@ -0,0 +1,10 @@
+const functions = require("firebase-functions");
+const { queryV2 } = require("./gameQuery");
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+
+exports.serversInfo = functions.https.onRequest(async (request, response) => {
+  const data = await queryV2();
+  response.send(data);
+});
